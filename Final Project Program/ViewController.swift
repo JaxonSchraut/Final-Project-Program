6e20eeb6b7ea38c43cff62b4b1187d93bf02a8ee
Ethan <ethan.laudick24@d155.org>
2022-12-02 07:30:33 -0600

    added test variable

--- Final Project Program/ViewController.swift
+++ Final Project Program/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var x = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
